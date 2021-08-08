0f84d82fd7c31506029dcc5cf987e50bd62d3a35
Artur Stambultsian <bumbledos@gmail.com>
2021-08-08 13:32:27 +0300

    fixed port

--- src/server/index.jsx
+++ src/server/index.jsx
@@ -9,7 +9,7 @@ import {getUsers} from '../api';
 import { DataProvider } from '../DataProvider';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const appRoot = process.cwd();
 
 const staticFiles = JSON.parse(fs.readFileSync(path.resolve(appRoot, './static/manifest.json')));
